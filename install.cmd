782aab1919b2a48ae0b7f50852fba04ce0130e83
Shan Ali <shan.ali.ru@gmail.com>
2023-09-03 15:14:00 -0400

    Create install.cmd

---
+++ install.cmd
@@ -0,0 +1,14 @@
+# choco-install-packages
+
+# install packages
+choco install googlechrome
+choco install python
+choco install git
+choco install microsoft-windows-terminal
+choco install lghub
+choco install obs-studio
+choco install vlc
+choco install vscode
+
+# upgrade all
+choco upgrade all

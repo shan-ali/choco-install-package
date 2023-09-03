c15863dbe11e6f0f453a043426fe66e2febeb0e5
Shan Ali <shan.ali.ru@gmail.com>
2023-09-03 16:01:28 -0700

    Update install-packages.cmd

--- install-packages.cmd
+++ install-packages.cmd
@@ -1,14 +1,14 @@
 # choco-install-packages
 
 # install packages
-choco install googlechrome
-choco install python
-choco install git
-choco install microsoft-windows-terminal
-choco install lghub
-choco install obs-studio
-choco install vlc
-choco install vscode
+choco install -y googlechrome
+choco install -y python
+choco install -y git
+choco install -y microsoft-windows-terminal
+choco install -y lghub
+choco install -y obs-studio
+choco install -y vlc
+choco install -y vscode
 
 # upgrade all
-choco upgrade all
+choco upgrade all -y
